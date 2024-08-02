ced9790ecedacf1dd1dc33066e5bd885b82054c3
Will Mobley <wmobley@tacc.utexas.edu>
2024-08-02 11:40:11 -0500

    Update header

--- src/Header.js
+++ src/Header.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import Login from './login';
+import favicon from './favicon.png';
 
 function Header() {
   return (
     <header className="app-header">
+      <img src={favicon} alt="Site Stories Icon" className="header-icon" />
       <h1>Site Stories</h1>
       <Login />
     </header>
